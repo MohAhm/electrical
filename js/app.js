2ffd53d85cdcb9cc6dc9103394f9fdcadeb6b538
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2019-10-19 23:58:33 +0200

    Refactor sections

--- js/app.js
+++ js/app.js
@@ -2,27 +2,42 @@ $('#toggle').click(function () {
     $(this).next('.nav').toggleClass("is-collapsed-mobile");
 });
 
+const md = window.matchMedia("(max-width: 768px)");
 
-$(window).scroll(function () {
+function blackBg() {
+    $('.main-header').css('padding', '20px 0');
+    $('.main-header').css('background-color', 'rgba(0, 0, 0, 0.4)');
+    $('.main-header').css('box-shadow', 'none');
+    $('.logo-title').css('color', 'white');
+    $('.nav__link a').css('color', 'white');
+    $('.nav__link--current a').css('color', 'white');
+}
 
-    if ($(window).width() > 768) {
+function whiteBg() {
+    $('.main-header').css('padding', '5px 0');
+    $('.main-header').css('background-color', 'white');
+    $('.main-header').css('box-shadow', '0 1px 4px rgba(0, 0, 0, .4)');
+    $('.logo-title').css('color', '#1C1C1C');
+    $('.nav__link a').css('color', '#1C1C1C');
+    $('.nav__link--current a').css('color', '#1C1C1C');
+}
+
+function navbarBg(md) {
+    $(window).scroll(function () {
+
         if ($(window).scrollTop() >= 100) {
-            $('.main-header').css('padding', '5px 0');
-            $('.main-header').css('background-color', 'white');
-            $('.main-header').css('box-shadow', '0 1px 4px rgba(0, 0, 0, .4)');
-            $('.logo-title').css('color', '#1C1C1C');
-            $('.nav__link a').css('color', '#1C1C1C');
-            $('.nav__link--current a').css('color', '#1C1C1C');
-            $('.nav__link--current a').css('border-bottom-color', '#1C1C1C');
+            whiteBg();
         } else {
-            $('.main-header').css('padding', '20px 0');
-            $('.main-header').css('background-color', 'rgba(0, 0, 0, 0.4)');
-            $('.main-header').css('box-shadow', 'none');
-            $('.logo-title').css('color', 'white');
-            $('.nav__link a').css('color', 'white');
-            $('.nav__link--current a').css('color', 'white');
-            $('.nav__link--current a').css('border-bottom-color', 'white');
+            if (md.matches) {
+                whiteBg();
+            } else {
+                blackBg();
+            }
         }
-    }
 
-});
+    });
+
+}
+
+navbarBg(md);
+md.addListener(navbarBg);
