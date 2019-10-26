76c896d45a821bebd0560b5e080e729f58f68464
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2019-10-26 10:57:42 +0200

    Add slick to the background images

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@ $('#toggle').click(function () {
     $(this).next('.nav').toggleClass("is-collapsed-mobile");
 });
 
+$('.banner .slides').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    arrows: false,
+    dots: true
+});
+
 $('.sponsors .slides').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
